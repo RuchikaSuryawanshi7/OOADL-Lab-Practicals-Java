88a4a8c8e07c3bd8315c716516393d087e47e208
Ruchika Suryawanshi <ruchikasuryawanshi710@gmail.com>
2022-01-21 21:48:18 +0530

    problem 1

--- Java-Basic-Understanding/src/Assignments/Assignment_3/MaxMin.java
+++ Java-Basic-Understanding/src/Assignments/Assignment_3/MaxMin.java
@@ -5,6 +5,25 @@ import java.util.Scanner;
 //Define two methods to print the maximum and the minimum number respectively
 // among three numbers entered by the user.
 public class MaxMin {
+    static int findLargest(int no1, int no2, int no3){
+        int max = Math.max(no1, no2);
+        if(max > no3){
+            return  max;
+        }else {
+            max = no3;
+            return max;
+        }
+    }
+    static int findSmallest(int no1, int no2, int no3){
+        int min = Math.min(no1, no2);
+        if(min < no3){
+            return min;
+        }else {
+            min = no3;
+            return min;
+        }
+
+    }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter no 1:");
@@ -13,6 +32,8 @@ public class MaxMin {
         int no2 = input.nextInt();
         System.out.println("Enter no 3:");
         int no3 = input.nextInt();
+        System.out.println("Largest number is: "+ findLargest(no1, no2, no3));
+        System.out.println("Smallest number is: "+ findSmallest(no1, no2, no3));
 
     }
 }

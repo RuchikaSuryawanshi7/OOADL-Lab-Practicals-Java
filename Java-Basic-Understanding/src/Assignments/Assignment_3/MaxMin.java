f4a75bd240c1e3743fad7cf07ba9ae54c579ab6b
Ruchika Suryawanshi <ruchikasuryawanshi710@gmail.com>
2022-01-21 21:17:28 +0530

    Assignment2 completed

---
+++ Java-Basic-Understanding/src/Assignments/Assignment_3/MaxMin.java
@@ -0,0 +1,18 @@
+package Assignments.Assignment_3;
+
+import java.util.Scanner;
+
+//Define two methods to print the maximum and the minimum number respectively
+// among three numbers entered by the user.
+public class MaxMin {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter no 1:");
+        int no1 = input.nextInt();
+        System.out.println("Enter no 2:");
+        int no2 = input.nextInt();
+        System.out.println("Enter no 3:");
+        int no3 = input.nextInt();
+
+    }
+}

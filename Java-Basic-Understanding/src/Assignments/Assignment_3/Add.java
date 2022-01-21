ce8e233f3575bb88b5e1e79ba2731fb89893deb4
Ruchika Suryawanshi <ruchikasuryawanshi710@gmail.com>
2022-01-21 22:06:12 +0530

    problem 3

---
+++ Java-Basic-Understanding/src/Assignments/Assignment_3/Add.java
@@ -0,0 +1,19 @@
+package Assignments.Assignment_3;
+
+import java.util.Scanner;
+
+//Write a program to print the sum of two numbers entered by user by defining your own method.
+public class Add {
+    static int Sum(int no1, int no2){
+        return  no1 + no2;
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter No 1");
+        int no1 = input.nextInt();
+        System.out.println("Enter No 2");
+        int no2 = input.nextInt();
+        System.out.println("Addition of two numbers is " + Sum(no1, no2));
+
+    }
+}
